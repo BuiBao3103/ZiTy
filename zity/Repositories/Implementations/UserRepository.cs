6aaafa5ec9d89763d2b292ca0b63c5b484e47f87
Bảo Bùi <hongbao2003@gmail.com>
2024-09-27 02:42:40 +0700

    feat: api feature sort

--- zity/Repositories/Implementations/UserRepository.cs
+++ zity/Repositories/Implementations/UserRepository.cs
@@ -19,13 +19,34 @@ namespace ZiTy.Repositories.Implementations
         public async Task<PaginatedResult<User>> GetAllAsync(UserQueryDto query)
         {
             var usersQuery = _dbContext.Users.Where(u => u.DeletedAt == null);
+
+            // Apply sorting
+            if (!string.IsNullOrEmpty(query.Sort))
+            {
+                var sortExpressions = query.Sort.Split(',');
+                foreach (var sortExpression in sortExpressions)
+                {
+                    var trimmedExpression = sortExpression.Trim();
+                    if (string.IsNullOrEmpty(trimmedExpression)) continue;
+
+                    var isDescending = trimmedExpression.StartsWith("-");
+                    var propertyName = isDescending ? trimmedExpression.Substring(1) : trimmedExpression;
+
+                    usersQuery = isDescending
+                        ? usersQuery.OrderByDescending(e => EF.Property<object>(e, propertyName))
+                        : usersQuery.OrderBy(e => EF.Property<object>(e, propertyName));
+                }
+            }
+
             var totalItems = await usersQuery.CountAsync();
             var users = await usersQuery
                 .Skip((query.Page - 1) * query.PageSize)
                 .Take(query.PageSize)
                 .ToListAsync();
+
             return new PaginatedResult<User>(users, totalItems, query.Page, query.PageSize);
         }
+
 
     }
 }
