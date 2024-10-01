7a78de1aadfe02d70205e5ed066bc055674fb435
MaiTrungChinhh <maitrungchinhnh1@gmail.com>
2024-10-01 22:01:14 +0700

    Refactor DTO classes to include missing properties and relationships

--- zity/DTOs/Questions/QuestionDto.cs
+++ zity/DTOs/Questions/QuestionDto.cs
@@ -1,6 +1,27 @@
-﻿namespace zity.DTOs.Questions
+﻿using zity.DTOs.Answers;
+using zity.DTOs.OtherAnswers;
+using zity.DTOs.Surveys;
+
+namespace zity.DTOs.Questions
 {
     public class QuestionDTO
     {
+        public int Id { get; set; }
+
+        public string Content { get; set; } = null!;
+
+        public DateTime CreatedAt { get; set; }
+
+        public DateTime? UpdatedAt { get; set; }
+
+        public DateTime? DeletedAt { get; set; }
+
+        public int? SurveyId { get; set; }
+
+        public ICollection<AnswerDTO> Answers { get; set; } = [];
+
+        public ICollection<OtherAnswerDTO> OtherAnswers { get; set; } = [];
+
+        public SurveyDTO? Survey { get; set; }
     }
 }
