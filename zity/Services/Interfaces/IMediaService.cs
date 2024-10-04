b1b9ec65c5071489b2482fa2603072540b1de554
Bảo Bùi <hongbao2003@gmail.com>
2024-10-04 17:41:02 +0700

    refactor: Configure Cloudinary for image upload and deletion

---
+++ zity/Services/Interfaces/IMediaService.cs
@@ -0,0 +1,8 @@
+﻿namespace zity.Services.Interfaces
+{
+    public interface IMediaService
+    {
+        Task DeleteImageAsync(string url, string folder);
+        Task<string> UploadImageAsync(IFormFile file, string folder);
+    }
+}
