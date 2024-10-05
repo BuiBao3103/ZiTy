00922cc5a9ced5906675876a5539511622c8466f
PhatJack <tienphatng.693@gmail.com>
2024-10-05 16:23:00 +0700

    refactor: Add survey API endpoints

--- zity-web/src/features/survey/surveySlice.ts
+++ zity-web/src/features/survey/surveySlice.ts
@@ -19,5 +19,37 @@ const surveySlice = createSlice({
   },
 })
 
+const surveyApiSlice = apiSlice.injectEndpoints({
+  endpoints: (builder) => ({
+    getSurverys: builder.query<void, void>({
+      query: () => '/survey',
+    }),
+    getSurveyById: builder.query<void, string>({
+      query: (id) => `/survey/${id}`,
+    }),
+    createSurvey: builder.mutation<void, void>({
+      query: (data) => ({
+        url: '/survey',
+        method: 'POST',
+        body: data,
+      }),
+    }),
+    editSurvery: builder.mutation<void, { id: string; data: void }>({
+      query: (data) => ({
+        url: `/survey/${data.id}`,
+        method: 'PUT',
+        body: data,
+      }),
+    }),
+    deleteSurvey: builder.mutation<void, string>({
+      query: (id) => ({
+        url: `/survey/${id}`,
+        method: 'DELETE',
+      }),
+    }),
+  }),
+})
+
 export const { createNewSurvey } = surveySlice.actions
 export default surveySlice.reducer
+export const { useGetSurveyByIdQuery } = surveyApiSlice
--- zity-web/src/pages/admin/apartment/components/apartment-list.tsx
+++ zity-web/src/pages/admin/apartment/components/apartment-list.tsx
@@ -6,7 +6,7 @@ const ApartmentList = () => {
   const navigate = useNavigate()
 
   return (
-    <div className="w-full mt-6 flex flex-col gap-4">
+    <div className="w-full flex flex-col gap-4">
       <div className="w-full p-4 border rounded-md bg-zinc-100">
         <h1 className="text-xl font-bold">Floor 1</h1>
         <div className="w-full flex gap-4 mt-4 overflow-x-auto">
