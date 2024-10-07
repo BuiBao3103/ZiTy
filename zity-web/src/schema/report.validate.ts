96698d9ac24545aafa6db082deb30a5d1886ea85
PhatJack <tienphatng.693@gmail.com>
2024-10-07 20:39:22 +0700

    feat: Add support for radio group component and add report admin page

--- zity-web/src/schema/report.validate.ts
+++ zity-web/src/schema/report.validate.ts
@@ -1,17 +1,22 @@
 import { ReportStatusSchema } from '@/enums'
 import { z } from 'zod'
+import { UserSchema } from './user.validate'
 
 export const ReportSchema = z.object({
   id: z.number(),
   content: z.string(),
   title: z.string(),
   status: ReportStatusSchema,
-	created_at: z.date(),
-  relationship_id: z.number().nullable(),
+  user: UserSchema.pick({ full_name: true, avatar: true }).partial(),
+  created_at: z.date(),
+  relationship_id: z.number().nullable().optional(),
 })
+export type Report = z.infer<typeof ReportSchema>
 
 export const RejectionReasonsSchema = z.object({
   id: z.number(),
   content: z.string(),
   report_id: z.number().nullable(),
 })
+
+export type RejectionReasons = z.infer<typeof RejectionReasonsSchema>
