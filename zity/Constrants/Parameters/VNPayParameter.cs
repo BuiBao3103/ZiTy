39a72d5b84b330ffe34e2c7ba7572e0fee306d74
Bảo Bùi <hongbao2003@gmail.com>
2024-10-13 03:24:47 +0700

    feat: Add VNPayParameter class for VNPay integration

---
+++ zity/Constrants/Parameters/VNPayParameter.cs
@@ -0,0 +1,22 @@
+﻿namespace zity.Constraints.Parameters
+{
+    public static class VNPayParameter
+    {
+        public const string VNP_VERSION = "vnp_Version";
+        public const string VNP_COMMAND = "vnp_Command";
+        public const string VNP_TMN_CODE = "vnp_TmnCode";
+        public const string VNP_AMOUNT = "vnp_Amount";
+        public const string VNP_BANK_CODE = "vnp_BankCode";
+        public const string VNP_CREATE_DATE = "vnp_CreateDate";
+        public const string VNP_CURR_CODE = "vnp_CurrCode";
+        public const string VNP_IP_ADDR = "vnp_IpAddr";
+        public const string VNP_LOCALE = "vnp_Locale";
+        public const string VNP_ORDER_INFO = "vnp_OrderInfo";
+        public const string VNP_ORDER_TYPE = "vnp_OrderType";
+        public const string VNP_RETURN_URL = "vnp_ReturnUrl";
+        public const string VNP_EXPIRE_DATE = "vnp_ExpireDate";
+        public const string VNP_TXN_REF = "vnp_TxnRef";
+        public const string VNP_SECURE_HASH = "vnp_SecureHash";
+    }
+
+}
