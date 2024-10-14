9d96063d99a0a9116f69c47f08a3021dac57e7a7
HoangGiaBao212 <hoanggiabao2122003@gmail.com>
2024-10-14 19:19:58 +0700

    Refactor ReportMapper to use AutoMapper for mapping between models and DTOs

---
+++ zity/Mappers/ReportMapping.cs
@@ -0,0 +1,23 @@
+﻿using AutoMapper;
+using zity.DTOs.Reports;
+using zity.Models;
+
+namespace zity.Mappers
+{
+    public class ReportMapping : Profile
+    {
+        public ReportMapping()
+        {
+            CreateMap<Report, ReportDTO>();
+
+            CreateMap<ReportCreateDTO, Report>()
+                .ForMember(dest => dest.CreatedAt, opt => opt.MapFrom(src => DateTime.Now));
+
+            CreateMap<ReportUpdateDTO, Report>()
+                .ForMember(dest => dest.UpdatedAt, opt => opt.MapFrom(src => DateTime.Now));
+
+            CreateMap<ReportPatchDTO, Report>()
+                .ForMember(dest => dest.UpdatedAt, opt => opt.MapFrom(src => DateTime.Now));
+        }
+    }
+}
--- zity/Services/Implementations/ReportService.cs
+++ zity/Services/Implementations/ReportService.cs
@@ -1,36 +1,41 @@
-﻿using zity.DTOs.Reports;
-using zity.Mappers;
+﻿using AutoMapper;
+using zity.DTOs.Reports;
+using zity.Models;
 using zity.Repositories.Interfaces;
 using zity.Services.Interfaces;
 using zity.Utilities;
 
 namespace zity.Services.Implementations
 {
-    public class ReportService(IReportRepository reportRepository) : IReportService
+    public class ReportService(IReportRepository reportRepository, IMapper mapper) : IReportService
     {
+        private readonly IMapper _mapper = mapper;
         private readonly IReportRepository _reportRepository = reportRepository;
 
         public async Task<PaginatedResult<ReportDTO>> GetAllAsync(ReportQueryDTO queryParam)
         {
             var pageReports = await _reportRepository.GetAllAsync(queryParam);
+            var reports = pageReports.Contents.Select(_mapper.Map<ReportDTO>).ToList();
+
             return new PaginatedResult<ReportDTO>(
-                pageReports.Contents.Select(ReportMapper.ReportToDTO).ToList(),
+                reports,
                 pageReports.TotalItems,
                 pageReports.Page,
                 pageReports.PageSize);
         }
 
-
         public async Task<ReportDTO?> GetByIdAsync(int id, string? includes)
         {
             var report = await _reportRepository.GetByIdAsync(id, includes);
-            return report != null ? ReportMapper.ReportToDTO(report) : null;
+            return report != null ? _mapper.Map<ReportDTO>(report) : null;
         }
+
         public async Task<ReportDTO> CreateAsync(ReportCreateDTO createDTO)
         {
-            var report = ReportMapper.ToModelFromCreate(createDTO);
-            return ReportMapper.ReportToDTO(await _reportRepository.CreateAsync(report));
+            var report = _mapper.Map<Report>(createDTO);
+            return _mapper.Map<ReportDTO>(await _reportRepository.CreateAsync(report));
         }
+
         public async Task<ReportDTO?> UpdateAsync(int id, ReportUpdateDTO updateDTO)
         {
             var existingReport = await _reportRepository.GetByIdAsync(id, null);
@@ -39,9 +44,9 @@ namespace zity.Services.Implementations
                 return null;
             }
 
-            ReportMapper.UpdateModelFromUpdate(existingReport, updateDTO);
+            _mapper.Map(updateDTO, existingReport);
             var updatedReport = await _reportRepository.UpdateAsync(existingReport);
-            return ReportMapper.ReportToDTO(updatedReport);
+            return _mapper.Map<ReportDTO>(updatedReport);
         }
 
         public async Task<ReportDTO?> PatchAsync(int id, ReportPatchDTO patchDTO)
@@ -52,9 +57,9 @@ namespace zity.Services.Implementations
                 return null;
             }
 
-            ReportMapper.PatchModelFromPatch(existingReport, patchDTO);
+            _mapper.Map(patchDTO, existingReport);
             var patchedReport = await _reportRepository.UpdateAsync(existingReport);
-            return ReportMapper.ReportToDTO(patchedReport);
+            return _mapper.Map<ReportDTO>(patchedReport);
         }
 
         public async Task<bool> DeleteAsync(int id)
