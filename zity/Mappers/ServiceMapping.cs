659dd5dec865f982046b2e01f3d4c3eb629a38cf
HoangGiaBao212 <hoanggiabao2122003@gmail.com>
2024-10-14 19:25:26 +0700

    Refactor ServiceMapper to use AutoMapper for mapping between models and DTOs

---
+++ zity/Mappers/ServiceMapping.cs
@@ -0,0 +1,23 @@
+﻿using AutoMapper;
+using zity.DTOs.Services;
+using zity.Models;
+
+namespace zity.Mappers
+{
+    public class ServiceMapping : Profile
+    {
+        public ServiceMapping()
+        {
+            CreateMap<Service, ServiceDTO>();
+
+            CreateMap<ServiceCreateDTO, Service>()
+                .ForMember(dest => dest.CreatedAt, opt => opt.MapFrom(src => DateTime.Now));
+
+            CreateMap<ServiceUpdateDTO, Service>()
+                .ForMember(dest => dest.UpdatedAt, opt => opt.MapFrom(src => DateTime.Now));
+
+            CreateMap<ServicePatchDTO, Service>()
+                .ForMember(dest => dest.UpdatedAt, opt => opt.MapFrom(src => DateTime.Now));
+        }
+    }
+}
