176186c3303297954b80d4d2621a031fc0c7be79
HoangGiaBao212 <hoanggiabao2122003@gmail.com>
2024-10-14 18:56:17 +0700

    Refactor ApartmentMapper to use AutoMapper for mapping between models and DTOs

---
+++ zity/Mappers/ApartmentMapping.cs
@@ -0,0 +1,23 @@
+﻿using AutoMapper;
+using zity.DTOs.Apartments;
+using zity.Models;
+
+namespace zity.Mappers
+{
+    public class ApartmentMapping : Profile
+    {
+        public ApartmentMapping()
+        {
+            CreateMap<Apartment, ApartmentDTO>();
+
+            CreateMap<ApartmentCreateDTO, Apartment>()
+                .ForMember(dest => dest.CreatedAt, opt => opt.MapFrom(src => DateTime.Now));
+
+            CreateMap<ApartmentUpdateDTO, Apartment>()
+                .ForMember(dest => dest.UpdatedAt, opt => opt.MapFrom(src => DateTime.Now));
+
+            CreateMap<ApartmentPatchDTO, Apartment>()
+                .ForMember(dest => dest.UpdatedAt, opt => opt.MapFrom(src => DateTime.Now));
+        }
+    }
+}
