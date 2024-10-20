5fa3bdf935552f0edf2287b6c4f6732fc18664d4
PhatJack <tienphatng.693@gmail.com>
2024-10-20 13:31:45 +0700

    feat: Add apartmentSlice with CRUD operations and create BaseEntity for all interface

---
+++ zity-web/src/features/apartment/apartmentSlice.ts
@@ -0,0 +1,59 @@
+import { apiSlice } from '../api/apiSlice'
+import { Apartment } from '@/schema/apartment.validate'
+
+export const apartmentSlice = apiSlice.injectEndpoints({
+  endpoints: (builder) => ({
+    getApartments: builder.query<ResponseDataType<Apartment>, number | void>({
+      query: (page = 1) => {
+        return {
+          url: `apartments?page=${page}`,
+        }
+      },
+      providesTags: (result) =>
+        result
+          ? [
+              ...result.contents.map(({ id }) => ({
+                type: 'Apartments' as const,
+                id,
+              })),
+              { type: 'Apartments', id: 'LIST' },
+            ]
+          : [{ type: 'Apartments', id: 'LIST' }],
+    }),
+    getApartment: builder.query<Apartment, string>({
+      query: (id: string) => ({
+        url: `apartments/${id}`,
+        method: 'GET',
+      }),
+      providesTags: (result, error, id) =>
+        result ? [{ type: 'Apartments', id }] : [],
+    }),
+    updateApartment: builder.mutation<
+      void,
+      { id: string; body: Partial<Apartment> }
+    >({
+      query: (data) => ({
+        url: `apartments/${data.id}`,
+        method: 'PUT',
+        body: data.body,
+      }),
+      invalidatesTags: (result, error, arg) => [
+        { type: 'Apartments', id: arg.id },
+      ],
+    }),
+    deleteApartment: builder.mutation<void, string | undefined>({
+      query: (id: string) => ({
+        url: `apartments/${id}`,
+        method: 'DELETE',
+      }),
+      invalidatesTags: (result, error, id) => [{ type: 'Apartments', id }],
+    }),
+  }),
+})
+
+export const {
+  useGetApartmentsQuery,
+  useGetApartmentQuery,
+  useUpdateApartmentMutation,
+  useDeleteApartmentMutation,
+} = apartmentSlice
