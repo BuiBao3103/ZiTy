49e8af5bd3fce3d7e9316b233be4eb49c49a45b8
Bảo Bùi <hongbao2003@gmail.com>
2024-11-02 01:44:51 +0700

    chore: Add email, media, Momo, SMS, and VNPay service interfaces

---
+++ Application/Core/Services/IMomoService.cs
@@ -0,0 +1,9 @@
+﻿using Application.DTOs.Momo;
+using Domain.Entities;
+
+namespace Application.Core.Services;
+
+public interface IMomoService
+{
+    Task<MomoCreatePaymentDto> CreatePaymentAsync(Bill bill, MomoRequestCreatePaymentDto request);
+}
---
+++ Application/DTOs/Momo/MomoCreatePaymentDto.cs
@@ -0,0 +1,11 @@
+namespace Application.DTOs.Momo;
+
+public class MomoCreatePaymentDto
+{
+    public string RequestId { get; set; }
+    public int ErrorCode { get; set; }
+    public string OrderId { get; set; }
+    public string Message { get; set; }
+    public string PayUrl { get; set; }
+}
+
