93e34426611809c1be52ed413685dbc4df1815a3
Bảo Bùi <hongbao2003@gmail.com>
2024-11-01 13:48:19 +0700

    build: init infrastrure

--- Domain/Core/Repositories/IUnitOfWork.cs
+++ Domain/Core/Repositories/IUnitOfWork.cs
@@ -1,9 +1,4 @@
 ﻿using Domain.Core.Models;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Domain.Core.Repositories;
 public interface IUnitOfWork
---
+++ Infrastructure/ServiceExtensions.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using Application.Core.Services;
+using Domain.Core.Repositories;
+using MyApp.Infrastructure.Data;
+using MyApp.Infrastructure.Repositories;
+using MyApp.Infrastructure.Services;
+
+namespace Infrastructure;
+public static class ServiceExtensions
+{
+    //public static void ConfigureInfrastructure(this IServiceCollection services)
+    //{
+    //    services.AddDbContext<MyAppDbContext>(options =>
+    //        options.UseSqlServer("name=ConnectionStrings:MyAppDatabase",
+    //        x => x.MigrationsAssembly("MyApp.Infrastructure")));
+
+    //    services.AddScoped(typeof(IBaseRepositoryAsync<>), typeof(BaseRepositoryAsync<>));
+    //    services.AddScoped<IUnitOfWork, UnitOfWork>();
+
+    //    services.AddScoped<IEmailService, EmailService>();
+    //    services.AddScoped<ILoggerService, LoggerService>();
+    //}
+
+    //public static void MigrateDatabase(this IServiceProvider serviceProvider)
+    //{
+    //    var dbContextOptions = serviceProvider.GetRequiredService<DbContextOptions<MyAppDbContext>>();
+
+    //    using (var dbContext = new MyAppDbContext(dbContextOptions))
+    //    {
+    //        dbContext.Database.Migrate();
+    //    }
+    //}
+}
