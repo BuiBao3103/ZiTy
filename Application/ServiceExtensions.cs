26ed4f3ca76341543a7bd145c980b94f2df494ea
MaiTrungChinhh <maitrungchinhnh1@gmail.com>
2024-11-03 23:55:54 +0700

    user

--- Application/ServiceExtensions.cs
+++ Application/ServiceExtensions.cs
@@ -3,18 +3,21 @@ using Application.Interfaces;
 using Application.Services;
 using Application.Mappers;
 
+
 namespace Application;
 public static class ServiceExtensions
 {
     public static void ConfigureApplication(this IServiceCollection services)
     {
         services.AddScoped<IAnswerService, AnswerService>();
+        services.AddScoped<IUserService, UserService>();
         services.AddScoped<IApartmentService, ApartmentService>();
         services.AddScoped<IAuthService, AuthService>();
         services.AddScoped<ISettingService, SettingService>();
         services.AddAutoMapper(
             typeof(AnswerMapping),
-            typeof(ApartmentMapping)
+            typeof(ApartmentMapping),
+            typeof(UserMapping)
         );
     }
 }
