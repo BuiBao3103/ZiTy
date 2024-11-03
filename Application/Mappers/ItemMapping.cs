b1a3af271a8e0f61e7c9a2fabbdd3fbccc82545d
HoangGiaBao212 <hoanggiabao2122003@gmail.com>
2024-11-03 16:36:01 +0700

    refactor: Simplify ItemMapping class structure and improve readability

--- Application/Mappers/ItemMapping.cs
+++ Application/Mappers/ItemMapping.cs
@@ -2,27 +2,26 @@
 using AutoMapper;
 using Domain.Entities;
 
-namespace Application.Mappers
+namespace Application.Mappers;
+
+public class ItemMapping : Profile
 {
-    public class ItemMapping : Profile
+    public ItemMapping()
     {
-        public ItemMapping()
-        {
-            CreateMap<Item, ItemDTO>();
-            // .ForMember(dest => dest.User, opt => opt.MapFrom(src => src.User));
+        CreateMap<Item, ItemDTO>();
+        // .ForMember(dest => dest.User, opt => opt.MapFrom(src => src.User));
 
-            CreateMap<ItemCreateDTO, Item>()
-                .ForMember(dest => dest.CreatedAt, opt => opt.MapFrom(src => DateTime.Now));
+        CreateMap<ItemCreateDTO, Item>()
+            .ForMember(dest => dest.CreatedAt, opt => opt.MapFrom(src => DateTime.Now));
 
-            CreateMap<ItemUpdateDTO, Item>()
-                .ForMember(dest => dest.UpdatedAt, opt => opt.MapFrom(src => DateTime.Now));
+        CreateMap<ItemUpdateDTO, Item>()
+            .ForMember(dest => dest.UpdatedAt, opt => opt.MapFrom(src => DateTime.Now));
 
-            CreateMap<ItemPatchDTO, Item>()
-                .ForMember(dest => dest.UpdatedAt, opt => opt.MapFrom(src => DateTime.Now))
-                .ForMember(dest => dest.Image, opt => opt.Condition(src => src.Image != null))
-                .ForMember(dest => dest.Description, opt => opt.Condition(src => src.Description != null))
-                .ForMember(dest => dest.IsReceive, opt => opt.Condition(src => src.IsReceive != null))
-                .ForMember(dest => dest.UserId, opt => opt.Condition(src => src.UserId != null));
-        }
+        CreateMap<ItemPatchDTO, Item>()
+            .ForMember(dest => dest.UpdatedAt, opt => opt.MapFrom(src => DateTime.Now))
+            .ForMember(dest => dest.Image, opt => opt.Condition(src => src.Image != null))
+            .ForMember(dest => dest.Description, opt => opt.Condition(src => src.Description != null))
+            .ForMember(dest => dest.IsReceive, opt => opt.Condition(src => src.IsReceive != null))
+            .ForMember(dest => dest.UserId, opt => opt.Condition(src => src.UserId != null));
     }
 }
