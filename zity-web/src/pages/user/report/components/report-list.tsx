b6cd0a954ccdf2633ae8f86f2dbf6c6033085db5
PhatJack <tienphatng.693@gmail.com>
2024-11-10 09:17:23 +0700

    feat: Refactor report components to use ReportItem for better code organization and readability

--- zity-web/src/pages/user/report/components/report-list.tsx
+++ zity-web/src/pages/user/report/components/report-list.tsx
@@ -5,15 +5,13 @@ import { BadgePlus } from 'lucide-react'
 import AlertDelete from '@/components/alert/AlertDelete'
 import { IReport } from '@/schema/report.validate'
 import { formatDateWithSlash } from '@/utils/Generate'
+import ReportItem from './report-item'
 
 interface ReportListProps {
   reports?: IReport[]
 }
 
 const ReportList = ({ reports }: ReportListProps) => {
-  const handleDelete = async () => {
-    console.log('delete')
-  }
 
   return (
     <div
@@ -26,48 +24,7 @@ const ReportList = ({ reports }: ReportListProps) => {
       </ReportForm>
       {reports &&
         reports.map((report, index) => (
-          <div
-            key={index}
-            className="p-4 w-full h-[310px] bg-white rounded-md flex flex-col gap-4 border">
-            <div className="w-full flex justify-between items-center">
-              <span className="font-medium">
-                {formatDateWithSlash(new Date(report.createdAt))}
-              </span>
-              <ReportForm mode="update" report={report}>
-                <Button variant={'default'} size={'sm'}>
-                  Details
-                </Button>
-              </ReportForm>
-            </div>
-            <div className="w-full h-full rounded-md bg-zinc-100 p-4 border border-zinc-300">
-              <p className="line-clamp-4 font-medium">
-                <span className="font-normal">Description:</span>{' '}
-                {report.content}
-              </p>
-            </div>
-            <div className="flex gap-2 uppercase">
-              <Badge variant={'info'}>Other</Badge>
-              <Badge variant={'error'}>Environment</Badge>
-              <Badge variant={'warning'}>Noise</Badge>
-            </div>
-            <div className="w-full flex justify-between items-center">
-              <div className="flex gap-2">
-                <div className="flex flex-col">
-                  <p className="font-medium">
-                    <span>{}</span>
-                  </p>
-                  <span className="text-sm font-medium ">
-                    {report.relationship?.apartmentId}
-                  </span>
-                </div>
-              </div>
-              <AlertDelete
-                description="report"
-                setAction={() => handleDelete()}
-                variants="error"
-              />
-            </div>
-          </div>
+          <ReportItem report={report} key={index} />
         ))}
     </div>
   )
