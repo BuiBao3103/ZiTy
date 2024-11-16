9dad844673356eef3d022bbd80a8df7b854bdadc
Bảo Bùi <hongbao2003@gmail.com>
2024-11-17 04:55:44 +0700

    feat: Add Billing.Domain project and related classes

---
+++ zity-server/Billing.Domain/Entities/Setting.cs
@@ -0,0 +1,22 @@
+﻿using Domain.Core.Models;
+using Domain.Enums;
+
+namespace Domain.Entities;
+public partial class Setting : BaseEntity
+{
+    public int Id { get; set; }
+
+    public string CurrentMonthly { get; set; } = null!;
+
+    public SystemStatusEnum SystemStatus { get; set; }
+
+    public float RoomPricePerM2 { get; set; }
+
+    public float WaterPricePerM3 { get; set; }
+
+    public float RoomVat { get; set; }
+
+    public int WaterVat { get; set; }
+
+    public int EnvProtectionTax { get; set; }
+}
