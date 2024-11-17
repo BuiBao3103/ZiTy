80a73d26e490eac42c224d179c841065c057f942
Bảo Bùi <hongbao2003@gmail.com>
2024-11-17 09:37:13 +0700

    feat: Add Billing.Infrastructure project and related classes

---
+++ zity-server/Billing.Infrastructure/Data/BillingDbContext.cs
@@ -0,0 +1,167 @@
+﻿using System;
+using System.Collections.Generic;
+using Billing.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Pomelo.EntityFrameworkCore.MySql.Scaffolding.Internal;
+
+
+namespace Billing.Infrastructure.Data;
+
+public partial class BillingDbContext : DbContext
+{
+    public BillingDbContext(DbContextOptions<BillingDbContext> options)
+        : base(options)
+    {
+    }
+
+    public virtual DbSet<Bill> Bills { get; set; }
+
+    public virtual DbSet<BillDetail> BillDetails { get; set; }
+
+    public virtual DbSet<Service> Services { get; set; }
+
+    public virtual DbSet<Setting> Settings { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        //if (!optionsBuilder.IsConfigured)
+        //{
+        //    optionsBuilder.LogTo(Console.WriteLine, LogLevel.None);
+        //}
+    }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder
+            .UseCollation("utf8mb4_0900_ai_ci")
+            .HasCharSet("utf8mb4");
+
+
+
+        modelBuilder.Entity<Bill>(entity =>
+        {
+            entity.HasKey(e => e.Id).HasName("PRIMARY");
+
+            entity.ToTable("bills");
+
+            entity.HasIndex(e => e.RelationshipId, "relationship_id");
+
+            entity.Property(e => e.Id).HasColumnName("id");
+            entity.Property(e => e.CreatedAt)
+                .HasColumnType("datetime")
+                .HasColumnName("created_at");
+            entity.Property(e => e.DeletedAt)
+                .HasColumnType("datetime")
+                .HasColumnName("deleted_at");
+            entity.Property(e => e.Monthly)
+                .HasMaxLength(7)
+                .HasColumnName("monthly");
+            entity.Property(e => e.NewWater).HasColumnName("new_water");
+            entity.Property(e => e.OldWater).HasColumnName("old_water");
+            entity.Property(e => e.RelationshipId).HasColumnName("relationship_id");
+            entity.Property(e => e.Status)
+                .HasDefaultValueSql("'UNPAID'")
+                .HasColumnType("enum('UNPAID','PAID','OVERDUE')")
+                .HasColumnName("status");
+            entity.Property(e => e.TotalPrice).HasColumnName("total_price");
+            entity.Property(e => e.UpdatedAt)
+                .HasColumnType("datetime")
+                .HasColumnName("updated_at");
+            entity.Property(e => e.WaterReadingDate)
+                .HasColumnType("datetime")
+                .HasColumnName("water_reading_date");
+
+
+        });
+
+        modelBuilder.Entity<BillDetail>(entity =>
+        {
+            entity.HasKey(e => e.Id).HasName("PRIMARY");
+
+            entity.ToTable("bill_details");
+
+            entity.HasIndex(e => e.BillId, "bill_id");
+
+            entity.HasIndex(e => e.ServiceId, "service_id");
+
+            entity.Property(e => e.Id).HasColumnName("id");
+            entity.Property(e => e.BillId).HasColumnName("bill_id");
+            entity.Property(e => e.CreatedAt)
+                .HasColumnType("datetime")
+                .HasColumnName("created_at");
+            entity.Property(e => e.DeletedAt)
+                .HasColumnType("datetime")
+                .HasColumnName("deleted_at");
+            entity.Property(e => e.Price).HasColumnName("price");
+            entity.Property(e => e.ServiceId).HasColumnName("service_id");
+            entity.Property(e => e.UpdatedAt)
+                .HasColumnType("datetime")
+                .HasColumnName("updated_at");
+
+            entity.HasOne(d => d.Bill).WithMany(p => p.BillDetails)
+                .HasForeignKey(d => d.BillId)
+                .OnDelete(DeleteBehavior.ClientSetNull)
+                .HasConstraintName("bill_details_ibfk_1");
+
+            entity.HasOne(d => d.Service).WithMany(p => p.BillDetails)
+                .HasForeignKey(d => d.ServiceId)
+                .OnDelete(DeleteBehavior.ClientSetNull)
+                .HasConstraintName("bill_details_ibfk_2");
+        });
+
+
+
+        modelBuilder.Entity<Service>(entity =>
+        {
+            entity.HasKey(e => e.Id).HasName("PRIMARY");
+
+            entity.ToTable("services");
+
+            entity.Property(e => e.Id).HasColumnName("id");
+            entity.Property(e => e.CreatedAt)
+                .HasColumnType("datetime")
+                .HasColumnName("created_at");
+            entity.Property(e => e.DeletedAt)
+                .HasColumnType("datetime")
+                .HasColumnName("deleted_at");
+            entity.Property(e => e.Description)
+                .HasMaxLength(255)
+                .HasColumnName("description");
+            entity.Property(e => e.Name)
+                .HasMaxLength(100)
+                .HasColumnName("name");
+            entity.Property(e => e.Price).HasColumnName("price");
+            entity.Property(e => e.UpdatedAt)
+                .HasColumnType("datetime")
+                .HasColumnName("updated_at");
+        });
+
+        modelBuilder.Entity<Setting>(entity =>
+        {
+            entity.HasKey(e => e.Id).HasName("PRIMARY");
+
+            entity.ToTable("settings");
+
+            entity.Property(e => e.Id)
+                .ValueGeneratedNever()
+                .HasColumnName("id");
+            entity.Property(e => e.CurrentMonthly)
+                .HasMaxLength(7)
+                .HasColumnName("current_monthly");
+            entity.Property(e => e.EnvProtectionTax).HasColumnName("env_protection_tax");
+            entity.Property(e => e.RoomPricePerM2).HasColumnName("room_price_per_m2");
+            entity.Property(e => e.RoomVat).HasColumnName("room_vat");
+            entity.Property(e => e.SystemStatus)
+                .HasColumnType("enum('PREPAYMENT','PAYMENT','OVERDUE','DELINQUENT')")
+                .HasColumnName("system_status");
+            entity.Property(e => e.WaterPricePerM3).HasColumnName("water_price_per_m3");
+            entity.Property(e => e.WaterVat).HasColumnName("water_vat");
+        });
+
+
+        OnModelCreatingPartial(modelBuilder);
+    }
+
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+}
---
+++ zity-server/Billing.Infrastructure/Services/MomoService.cs
@@ -0,0 +1,92 @@
+﻿using System.Security.Cryptography;
+using System.Text;
+using Billing.Application.Core.Services;
+using Billing.Application.DTOs.Momo;
+using Billing.Domain.Configurations;
+using Billing.Domain.Entities;
+using Newtonsoft.Json;
+using RestSharp;
+
+namespace Billing.Infrastructure.Services;
+public class MomoService(AppSettings appSettings) : IMomoService
+{
+    private readonly AppSettings _appSettings = appSettings;
+
+    public async Task<MomoCreatePaymentDto> CreatePaymentAsync(Bill bill, MomoRequestCreatePaymentDto request)
+    {
+        Console.WriteLine("Creating Momo payment...");
+        var billInfo = $"Payment for the bill {bill.Monthly}";
+        var requestId = Guid.NewGuid().ToString();
+        var orderId = $"{DateTime.Now.Ticks}_{bill.Id}";
+        var notifyUrl = _appSettings.MomoSettings.NotifyUrl.Replace("{{id}}", bill.Id.ToString());
+        // Tạo extraData
+        var extraData = Convert.ToBase64String(Encoding.UTF8.GetBytes(JsonConvert.SerializeObject(new { bill_id = bill.Id })));
+
+        // Xây dựng rawData với các thuộc tính theo đúng thứ tự a-z
+        var rawData =
+            $"accessKey={_appSettings.MomoSettings.AccessKey}&amount={bill.TotalPrice}&extraData={extraData}" +
+            $"&ipnUrl={notifyUrl}&orderId={orderId}&orderInfo={billInfo}" +
+            $"&partnerCode={_appSettings.MomoSettings.PartnerCode}&redirectUrl={_appSettings.MomoSettings.ReturnUrl}" +
+            $"&requestId={requestId}&requestType={request.RequestType}";
+
+        // Tạo chữ ký
+        var signature = ComputeHmacSha256(rawData, _appSettings.MomoSettings.SecretKey);
+
+        // Tạo RestClient và RestRequest
+        var client = new RestClient(_appSettings.MomoSettings.MomoApiUrl);
+        var restRequest = new RestRequest() { Method = Method.Post };
+        restRequest.AddHeader("Content-Type", "application/json; charset=UTF-8");
+
+        // Dữ liệu gửi lên API
+        var requestData = new
+        {
+            partnerCode = _appSettings.MomoSettings.PartnerCode,
+            requestId,
+            amount = (long)bill.TotalPrice,
+            orderId,
+            orderInfo = billInfo,
+            redirectUrl = _appSettings.MomoSettings.ReturnUrl,
+            ipnUrl = notifyUrl,
+            lang = "vi",
+            orderExpireTime = 15,
+            extraData,
+            requestType = request.RequestType,
+            signature,
+            autoCapture = true
+        };
+
+        restRequest.AddParameter("application/json", JsonConvert.SerializeObject(requestData), ParameterType.RequestBody);
+
+        try
+        {
+            var response = await client.ExecuteAsync(restRequest);
+            var momoCreatePaymentDto = JsonConvert.DeserializeObject<MomoCreatePaymentDto>(response.Content);
+            if (response.IsSuccessful)
+            {
+                return momoCreatePaymentDto;
+            }
+            else
+            {
+                // Log the error and throw an exception
+                Console.WriteLine($"Error: {momoCreatePaymentDto.Message}");
+                throw new Exception($"Failed to create Momo payment: {momoCreatePaymentDto.Message}");
+            }
+        }
+        catch (Exception ex)
+        {
+            // Log the exception
+            Console.WriteLine($"Exception: {ex.Message}");
+            throw;
+        }
+    }
+
+    // Phương thức tạo chữ ký HMAC-SHA256
+    private static string ComputeHmacSha256(string rawData, string secretKey)
+    {
+        using (var hmac = new HMACSHA256(Encoding.UTF8.GetBytes(secretKey)))
+        {
+            var hash = hmac.ComputeHash(Encoding.UTF8.GetBytes(rawData));
+            return BitConverter.ToString(hash).Replace("-", "").ToLower();
+        }
+    }
+}
