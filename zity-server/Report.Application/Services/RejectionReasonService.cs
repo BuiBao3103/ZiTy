7e311eca73d7f47fab0949f3a717df9b02286db6
TonyTrNhPh <trnhphquang190703@gmail.com>
2024-12-10 15:54:58 +0700

    refactor: Add GetAllAsync method in RejectionReasonService

--- zity-server/Report.Application/Services/RejectionReasonService.cs
+++ zity-server/Report.Application/Services/RejectionReasonService.cs
@@ -10,8 +10,28 @@ using Report.Domain.Exceptions;
 
 namespace Report.Application.Services;
 
-public class RejectionReasonService(IUnitOfWork unitOfWork, IMapper mapper) 
-{ 
-
+public class RejectionReasonService(IUnitOfWork unitOfWork, IMapper mapper) : IRejectionReasonService
+{
+    private readonly IMapper _mapper = mapper;
+    private readonly IUnitOfWork _unitOfWork = unitOfWork;
+    public async Task<PaginatedResult<RejectionReasonDTO>> GetAllAsync(RejectionReasonQueryDTO query)
+    {
+        var filterExpression = query.BuildFilterCriteria<RejectionReason>(a => a.DeletedAt == null);
+        var spec = new BaseSpecification<RejectionReason>(filterExpression);
+        var totalCount = await _unitOfWork.Repository<RejectionReason>().CountAsync(spec);
+        query.Includes?.Split(',').Select(i => char.ToUpper(i[0]) + i[1..]).ToList().ForEach(spec.AddInclude);
+        if (!string.IsNullOrEmpty(query.Sort))
+            if (query.Sort.StartsWith("-"))
+                spec.ApplyOrderByDescending(query.Sort[1..]);
+            else
+                spec.ApplyOrderBy(query.Sort);
+        spec.ApplyPaging(query.PageSize * (query.Page - 1), query.PageSize);
+        var data = await _unitOfWork.Repository<RejectionReason>().ListAsync(spec);
+        return new PaginatedResult<RejectionReasonDTO>(
+            data.Select(_mapper.Map<RejectionReasonDTO>).ToList(),
+            totalCount,
+            query.Page,
+            query.PageSize);
+    }
 }
 
